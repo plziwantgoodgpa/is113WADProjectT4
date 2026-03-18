26467feed09bd6cdcd9315ac825861fb4fe670a9
Yunfeng <20041027zz@gmail.com>
2026-03-18 09:34:36 +0800

    added some simple code in homeviews, setup for mongoDB

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
 const server = express();
 const homeRoutes = require("./routes/homeRoutes");
 
+// Specify the path to the environment variablef file 'config.env'
+dotenv.config({ path: './config.env' });
 server.use(express.urlencoded({ extended: true }));
 server.set("view engine", "ejs");
 
+//Routes
 server.use("/", homeRoutes);
 
-const hostname = "localhost";
-const port = 8000;
+// async function to connect to DB
+async function connectDB() {
+  try {
+    // connecting to Database with our config.env file and DB is constant in config.env
+    await mongoose.connect(process.env.DB);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+function startServer() {
+  const hostname = "localhost"; // Define server hostname
+  const port = 8000;// Define port number
+
+  // Start the server and listen on the specified hostname and port
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
+
+// call connectDB first and when connection is ready we start the web server
+connectDB().then(startServer);
