7ac1f67d8bd4274bed32070188ce389663d4c849
akheelthaha2025 <akheelt.b.2025@computing.smu.edu.sg>
2026-03-29 15:22:45 +0800

    need to implement

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const playlistRoutes = require("./routes/playlistRoutes");
 const homeRoutes = require("./routes/homeRoutes")
 const categoryRoutes = require("./routes/categoryRoutes")
 const session = require("express-session");
-// Specify the path to the environment variablef file 'config.env'
+// Specify the path to the environment variable file 'config.env'
 dotenv.config({ path: './config.env' });
 server.use(express.urlencoded({ extended: true }));
 server.set("view engine", "ejs");
